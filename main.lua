d3f4c4681c74e942c7a1b0f4071ac95af6e58712
mnhnctp88 <113150048+88mnhnctp88@users.noreply.github.com>
2024-07-13 13:51:49 +0330

    Create main.lua
    shut up

---
+++ main.lua
@@ -0,0 +1,25 @@
+function love.load()
+
+    
+    
+end
+
+
+function love.update(dt)
+
+    
+
+
+
+end
+
+
+function love.draw()
+    
+    love.graphics.setBackgroundColor(100/255,242/255,0/255)
+    love.graphics.setColor(240/255, 111/255, 255/255)
+    love.graphics.circle("fill",100, 400, 100)
+    love.graphics.setColor(11/255, 111/255, 255/255)
+    love.graphics.rectangle("fill", 200, 200, 100, 100)
+
+end
