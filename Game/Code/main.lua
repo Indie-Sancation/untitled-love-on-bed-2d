92793ae8d5d6ef64aa52d9e4daf9aa1fd8f1cc03
mnhnctp88 <a1.m2.h3.mine@gmail.com>
2024-07-13 16:39:44 +0330

    Update main.lua
    Adding so much i don't remember

--- Game/Code/main.lua
+++ Game/Code/main.lua
@@ -23,7 +23,6 @@ function love.update(dt)
 
 
 
-
 end
 
 
@@ -32,12 +31,22 @@ function love.draw()
     love.graphics.setColor(1,0,0)
     love.graphics.circle("fill", target.x, target.y, target.radius)
 
-    
-
     love.graphics.setFont(gamefontsize)
     love.graphics.setColor(1,1,1)
     love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 10) 
     love.graphics.print("Score:"..tostring(score), 10, 27)
 end
 
+function love.mousepressed( x, y, button, istouch, presses )
 
+    if button == 1 or 2 then
+        score = score + 1
+    end
+end
+
+
+
+
+function distanceBetween(x1, y1, x2, y2)
+   return math.sqrt(x2-x1)^2 + (y2-y1)^2
+end
