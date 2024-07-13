4905f39e510e0d2e1894202dfa04ebd4ba08c5a6
dadadadadadad3 <dari29903@gmail.com>
2024-07-13 16:34:57 +0330

    -

--- Game/Code/main.lua
+++ Game/Code/main.lua
@@ -21,7 +21,7 @@ end
 
 function love.update(dt)
 
-
+ 
 --25 million
 
 end
@@ -32,12 +32,11 @@ function love.draw()
     love.graphics.setColor(1,0,0)
     love.graphics.circle("fill", target.x, target.y, target.radius)
 
-    
 
+
+    
     love.graphics.setFont(gamefontsize)
     love.graphics.setColor(1,1,1)
     love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 10) 
     love.graphics.print("Score:"..tostring(score), 10, 27)
 end
-
-
